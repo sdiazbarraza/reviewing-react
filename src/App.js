b7c432ed628260388dcb0f8be469a096434199e5
sdiazbarraza <sebastiandesarrolla@gmail.com>
2024-10-12 19:28:41 +0000

    feature: share states between 2 button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Profile from './Profile'; // Importa el componente MyButton
 import MyButton from './MyButton';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="App">
         <Profile />
-        <MyButton />
-        <MyButton />
+        <MyButton count={count} handleClick={handleClick} />
+        <MyButton count={count} handleClick={handleClick} />
     </div>
   );
 }
--- src/MyButton.js
+++ src/MyButton.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
 
-function MyButton() {
-  const [count, setCount] = useState(0);
+function MyButton({count, handleClick}) {
 
-  function handleClick() {
-    setCount(count + 1);
-  }
-  
   return (
     <button onClick={handleClick}>Clicked {count} times</button>
   );
